54aec271d4d7b929baaf75dd8e07575f5a6eeaa3
Isaiah Murray <murray.isaiah13@gmail.com>
2020-07-16 11:25:04 -0400

    Adjusted particles amount to 80

--- particles.js
+++ particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 600
